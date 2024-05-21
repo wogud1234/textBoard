02a63534bdb592a2b204dd99ae6f6d96d552e713
wogud1234 <bexel963@gmail.com>
2024-05-21 21:45:15 +0900

    프로그램 시작 메시지 출력

--- src/main/java/com/wogud/board/Main.java
+++ src/main/java/com/wogud/board/Main.java
@@ -3,6 +3,9 @@ package com.wogud.board;
 public class Main {
     public static void main(String[] args) {
 
-
+        System.out.println("== 텍스트 게시판 v 0.1==");
+        System.out.println("프로그램 시작");
+        System.out.println("프로그램 끝");
+        System.out.println("== 게시판을 종료합니다.");
     }
 }
